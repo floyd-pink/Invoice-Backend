1ab9db783d42e3538441aeb3454c703ed0a4a46e
Aarogya <aarufloyd@gmail.com>
2026-07-22 23:13:08 +0545

    feat Added contoller,service and Module

--- src/invoice_items/invoice_items.controller.ts
+++ src/invoice_items/invoice_items.controller.ts
@@ -1,11 +1,24 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, Param } from '@nestjs/common';
 import { CreateInvoiceDto } from '../invoice_items/dto/invoice_items.dto';
 import { InvoiceItemsService } from './invoice_items.service';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
+import { ActiveUser } from 'src/common/decorators/active-user.decorator';
 @Controller('invoice-items')
 export class InvoiceItemsController {
   constructor(private readonly invoiceItemsService: InvoiceItemsService) {}
-  @Post('create-invoice')
-  async createInvoice(@Body() payload: CreateInvoiceDto) {
-    return this.invoiceItemsService.createInvoice(payload);
+  @Post('business/:businessId/create-invoice')
+  @UseGuards(JwtAuthGuard)
+  async createInvoice(
+    @ActiveUser() user: { sub: string },
+    @Body() invoice_payload: CreateInvoiceDto,
+    @Param('businessId') businessId: string,
+  ) {
+    const userId = Number(user.sub);
+
+    return this.invoiceItemsService.createInvoice(
+      invoice_payload,
+      businessId,
+      userId,
+    );
   }
 }
--- src/invoice_items/invoice_items.service.ts
+++ src/invoice_items/invoice_items.service.ts
@@ -1,19 +1,104 @@
-import { Injectable } from '@nestjs/common';
+import { BusinessCustomer } from 'src/business-customer/entities/business-customer.entity';
+import {
+  Injectable,
+  NotFoundException,
+  ForbiddenException,
+} from '@nestjs/common';
 import { CreateInvoiceDto } from '../invoice_items/dto/invoice_items.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { BusinessEntity } from 'src/bussiness/entities/bussiness.entity';
+import { DataSource } from 'typeorm';
+import { InvoiceEntity } from './entities/invoice.entity';
+import { InvoiceItemsEntity } from './entities/invoiceItems.entity';
+
 @Injectable()
 export class InvoiceItemsService {
-  private invoicesTable = [];
-  private invoiceItemsTable = [];
+  constructor(
+    @InjectRepository(BusinessEntity)
+    private readonly BusinessRepository: Repository<BusinessEntity>,
+    @InjectRepository(BusinessCustomer)
+    private readonly BusinessCustomerRepository: Repository<BusinessCustomer>,
+    private readonly dataSource: DataSource,
+  ) {}
+  async createInvoice(
+    payload: CreateInvoiceDto,
+    businessId: string,
+    userId: number,
+  ) {
+    const business = await this.BusinessRepository.findOne({
+      where: { business_id: businessId },
+      relations: { owner: true },
+    });
 
-  private nextInvoiceId = 1;
-  private nextInvoiceItemId = 1;
+    if (!business) {
+      throw new NotFoundException('Business not found');
+    }
 
-  constructor() {}
-  async createInvoice(payload: CreateInvoiceDto) {
-    const calculatedTotal = payload.items.reduce(
-      (sum, item) => sum + item.item_price * item.item_quantity,
+    if (business.owner.id !== userId) {
+      throw new ForbiddenException(
+        'you are not allowed to create invoice for this business',
+      );
+    }
+    const businessCustomer = await this.BusinessCustomerRepository.findOne({
+      where: { businessId: businessId, customerId: payload.customer_id },
+      relations: { customer: true },
+    });
+    if (!businessCustomer) {
+      throw new NotFoundException(
+        'This customer does not belong to your business',
+      );
+    }
+
+    const items = payload.items.map((item) => {
+      const totalPrice = item.item_price * item.item_quantity;
+      return {
+        ...item,
+        totalUnitPrice: totalPrice,
+      };
+    });
+    const totalAmount = items.reduce(
+      (sum, item) => sum + item.totalUnitPrice,
       0,
     );
-    return { message: 'Invoice created successfully', data: payload };
+    const paidAmount = payload.paid_amount ?? 0;
+    const dueAmount = totalAmount - paidAmount;
+
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      //first creating Invoice
+      const invoice = queryRunner.manager.create(InvoiceEntity, {
+        business,
+        customer: businessCustomer.customer,
+        totalAmount,
+        paidAmount,
+        dueAmount,
+        notes: payload.notes,
+      });
+      await queryRunner.manager.save(invoice);
+      //creating Invoice Items
+
+      const invoiceItems = items.map((item) =>
+        queryRunner.manager.create(InvoiceItemsEntity, {
+          invoice,
+          itemName: item.item_name,
+          itemQuantity: item.item_quantity,
+          unitPrice: item.item_price,
+          totalUnitPrice: item.totalUnitPrice,
+        }),
+      );
+      await queryRunner.manager.save(invoiceItems);
+
+      await queryRunner.commitTransaction();
+      return { message: 'Invoice created Successfully', invoice };
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      await queryRunner.release();
+    }
   }
 }
